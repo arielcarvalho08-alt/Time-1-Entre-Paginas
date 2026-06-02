2f4fe7d9dbd08d4bfb11f4b079948e94a1c2819f
alana-blip <arneildo.samuel11@aluno.ifce.edu.br>
2026-06-02 16:24:56 -0300

    inserindo componente visual LocalRowView com tags de status

---
+++ FinalProjectFoundation/LocalRowView.swift
@@ -0,0 +1,18 @@
+//
+//  LocalRowView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import SwiftUI
+
+struct LocalRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocalRowView()
+}
