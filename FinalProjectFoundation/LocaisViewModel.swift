39ddfa8eb7f2a27effaab4e214a480e9d010cae0
alana-blip <arneildo.samuel11@aluno.ifce.edu.br>
2026-06-09 17:22:12 -0300

    atualizando mapa para mostrar pins reais do SQLite

---
+++ FinalProjectFoundation/LocaisViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  LocaisViewModel.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import Foundation
+import Combine
+
+class LocaisViewModel: ObservableObject {
+    @Published var locais: [Local] = []
+    @Published var textoPesquisa: String = ""
+    @Published var apenasAbertos: Bool = false {
+        didSet { carregarDadosdoBanco() }
+    }
+    
+    private var todosLocais: [Local] = []
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        carregarDadosdoBanco()
+        
+        $textoPesquisa
+            .debounce(for: .milliseconds(300), scheduler: RunLoop.main)
+            .sink { [weak self] texto in
+                self?.filtrarLocais(por: texto)
+            }
+            .store(in: &cancellables)
+    }
+    
+    func carregarDadosdoBanco() {
+        let dadosDoBanco = DatabaseManager.shared.fetchLocais()
+        self.todosLocais = dadosDoBanco
+        self.locais = dadosDoBanco
+    }
+    
+    private func filtrarLocais(por texto: String) {
+        var filtrados = todosLocais
+        
+        if !texto.isEmpty {
+            filtrados = filtrados.filter {
+                $0.nome.localizedCaseInsensitiveContains(texto)  ||
+                $0.bairro.localizedCaseInsensitiveContains(texto)
+            }
+        }
+        
+        if apenasAbertos {
+            filtrados = filtrados.filter { $0.abertoAgora == true}
+        }
+        
+        self.locais = filtrados
+    }
+}
