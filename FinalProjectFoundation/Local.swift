ac23c753c414243ad8736706eb12a8fca440b30b
arielcarvalho08-alt <ariel.carvalho08@aluno.ifce.edu.br>
2026-06-09 17:21:40 -0300

    Adicionando tela de detalhes ao clicar no local

---
+++ FinalProjectFoundation/Local.swift
@@ -0,0 +1,52 @@
+//
+//  Local.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 02/06/26.
+//
+
+import Foundation
+import GRDB
+
+struct Local: Identifiable, FetchableRecord, TableRecord, Decodable {
+    let id: Int64?
+    let nome: String
+    let logradouro: String
+    let numero: String
+    let bairro: String
+    let latitude: Double?
+    let longitude: Double?
+    let distanciaSimulada: Double
+    let abertoAgora: Bool
+    
+    // Ajustado para bater exatamente com o nome da tabela no SQLiteOnline
+    static let databaseTableName = "Local"
+
+    // Mapeamento corrigido para bater com as colunas reais do banco
+    enum CodingKeys: String, CodingKey {
+        case id, nome, logradouro, numero, bairro, latitude, longitude
+        case distanciaSimulada
+        case abertoAgora
+    }
+
+    // Inicializador customizado para transformar o Int (0 ou 1) do banco em Bool no Swift
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try container.decodeIfPresent(Int64.self, forKey: .id)
+        self.nome = try container.decode(String.self, forKey: .nome)
+        self.logradouro = try container.decode(String.self, forKey: .logradouro)
+        self.numero = try container.decode(String.self, forKey: .numero)
+        self.bairro = try container.decode(String.self, forKey: .bairro)
+        self.latitude = try container.decodeIfPresent(Double.self, forKey: .latitude)
+        self.longitude = try container.decodeIfPresent(Double.self, forKey: .longitude)
+        self.distanciaSimulada = try container.decode(Double.self, forKey: .distanciaSimulada)
+        
+        // Tenta ler como Int (do SQLite) e converte para Bool
+        if let abertoInt = try? container.decode(Int.self, forKey: .abertoAgora) {
+            self.abertoAgora = (abertoInt == 1)
+        } else {
+            self.abertoAgora = try container.decode(Bool.self, forKey: .abertoAgora)
+        }
+    }
+}
--- FinalProjectFoundation/MainView.swift
+++ FinalProjectFoundation/MainView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var mostrandoFiltros = false
+    
+    @StateObject private var viewModel = LocaisViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            NavigationView {
+                
+                VStack{
+                    TextField("Pesquisar por nome ou bairro...", text: $viewModel.textoPesquisa)
+                        .padding(8)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                    List(viewModel.locais) { local in
+                        NavigationLink(destination: DetalhesView(nome: local.nome,
+                                                                 logradouro: local.logradouro,
+                                                                 numero: local.numero,
+                                                                 bairro: local.bairro)) {
+                            LocalRowView(nome: local.nome,
+                                         tipo: "Instituição",
+                                         logradouro: local.logradouro,
+                                         numero: local.numero,
+                                         bairro: local.bairro,
+                                         avaliacao: 4.7, abertoAgora: local.abertoAgora == true,
+                                         distancia: local.distanciaSimulada)
+                        }
+                    }
+                    .navigationTitle("Lista de locais")
+                    .navigationBarItems(trailing: Button(action: { mostrandoFiltros = true }) {
+                        Image(systemName: "line.3.horizontal.decrease.circle").font(.title2)
+                    })
+                }
+                .tabItem { Label("Locais", systemImage: "list.bullet") }
+                MapaView()
+                    .tabItem { Label("Explorar", systemImage: "map")}
+            }
+            .sheet(isPresented: $mostrandoFiltros) { FiltrosView() }
         }
-        .padding()
     }
 }
+    
+    //
+    //#Preview {
+    //    MainView()
+    //}
 
-#Preview {
-    MainView()
-}
